b5e8d68fd9d1f3ba1e8c19a43653725be41db332
mbesnili <besnilimustafa@gmail.com>
2018-07-24 11:08:21 +0300

    Fix scrollToBottomAnimated when collection view has more than one section.

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -414,7 +414,8 @@ JSQMessagesKeyboardControllerDelegate>
         return;
     }
     
-    NSIndexPath *lastCell = [NSIndexPath indexPathForItem:([self.collectionView numberOfItemsInSection:0] - 1) inSection:0];
+    NSUInteger lastSection = [self.collectionView numberOfSections] - 1;
+    NSIndexPath *lastCell = [NSIndexPath indexPathForItem:([self.collectionView numberOfItemsInSection:0] - 1) inSection:lastSection];
     [self scrollToIndexPath:lastCell animated:animated];
 }
 
@@ -443,7 +444,7 @@ JSQMessagesKeyboardControllerDelegate>
     }
 
     NSInteger item = MAX(MIN(indexPath.item, numberOfItems - 1), 0);
-    indexPath = [NSIndexPath indexPathForItem:item inSection:0];
+    indexPath = [NSIndexPath indexPathForItem:item inSection:indexPath.section];
 
     //  workaround for really long messages not scrolling
     //  if last message is too long, use scroll position bottom for better appearance, else use top
